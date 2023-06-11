315678334736cc3529f93a53683814c6e9c9ecec
NickBon4 <nick.bon4.work@gmail.com>
2023-06-11 20:29:57 +0300

    Last commit fix

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,7 @@
 const uniqid = require('uniqid')
 const cubes = []
 
-exports.getAll = () => db.cubes.slice()
+exports.getAll = () => cubes.slice()
 
 exports.create = (cubeData) => {
     const newCube ={
