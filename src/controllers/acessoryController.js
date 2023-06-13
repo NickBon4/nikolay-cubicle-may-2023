56bc1f425e574210b7f5724a4f5c0eea5f838426
NickBon4 <nick.bon4.work@gmail.com>
2023-06-13 18:53:24 +0300

    add post action for page accessory

--- src/controllers/acessoryController.js
+++ src/controllers/acessoryController.js
@@ -5,4 +5,13 @@ router.get('/create', (req, res) => {
     res.render('acessory/create')
 })
 
+router.post('/create', (req, res) => {
+    const body = req.body
+
+    console.log(body);
+    //TODO: add accessory data to db
+
+    res.redirect('/')
+})
+
 module.exports = router
