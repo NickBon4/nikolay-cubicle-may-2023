460b10b277dfbd8c4be8229ffc3b537e6f70911b
NickBon4 <nick.bon4.work@gmail.com>
2023-06-16 19:43:32 +0300

    Save user with validated password

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: {
+        type: String,
+        // validate:{
+        //     validator: function(value){
+        //         return this.repeatPassword === value 
+        //     },
+        //     message: `Invalid password!`
+        // }
+    },
+
+})
+
+userSchema.virtual('repeatPassword')
+    .set(function (value) {
+        if(value !== this.password){
+            throw new mongoose.MongooseError('Invalid password!')
+        }
+    })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
