167c94d8e4766d5e1a43342d645e8a9bef23e1b2
NickBon4 <nick.bon4.work@gmail.com>
2023-06-16 19:07:13 +0300

    Add register page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const acessoryController = require('./controllers/acessoryController')
+const userController = require('./controllers/userController')
 
 router.use(homeController);
 router.use('/cubes', cubeController);
 router.use('/acessories', acessoryController)
+router.use('/users', userController)
 router.get('*', (req, res) => {
     res.redirect('/404');
 });
